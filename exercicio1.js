98a05f0ddae9d46943dfda47f34792e3afecac3c
Irian Delgado <iriandelgado98@gmail.com>
2020-06-08 17:41:55 -0100

    func soma

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+let numero1 = parseInt(prompt("inserir numero 1"));
+let numero2 = parseInt(prompt("inserir numero 2"));
+
+
+function soma(numero1,numero2){
+    conta = numero1+numero2;
+    return conta
+}
+
+alert(soma(numero1,numero2));
+
